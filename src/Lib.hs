205f352e89c08f7dad6ee9f8416b6ea18c92aaed
Valentin Mosca <vmosca@frba.utn.edu.ar>
2025-04-28 12:31:38 -0300

    Update Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,28 @@ module Lib () where
 
 import Text.Show.Functions ()
 
-doble :: Int -> Int
-doble x = x * 2
+
+data Personajes = UnEquipo {
+                            nombre :: String,
+                            poderBasico :: Poder, 
+                            superPoder :: Poder, 
+                            poderActivo :: Bool, 
+                            cantidadDeVida :: Int
+                        } deriving Show 
+
+type Poder = Personajes -> Personajes
+
+espina :: Personajes
+espina  =  UnEquipo "Espina" bolaEspinosa granadaDeEspinas True 4800
+
+pamela :: Personajes
+pamela =  UnEquipo "Pamela" lluviaDeTuercas torretaCurativa False 9600
+
+--bolaEspinosa :: Poder 
+--bolaEspinosa unPersonaje = unPersonaje {cantidadDeVida - 1000} 
+
+granadaDeEspinas :: Int -> Poder
+granadaDeEspinas radio unPersonaje = unPersonaje 
+| unPersonaje radio > 3 = {nombre ++ "espina estuvo aqui"} 
+| unPersonaje cantidadDeVida < 800 =  {poderActivo = False, cantidadDeVida = 0}
+| otherwise = bolaEspinosa
